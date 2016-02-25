beeaa496c52d1dea22eab378f83aaf88c8447bb6
Federico Frenguelli <synasius@gmail.com>
2016-02-25 13:26:56 +0100

    fixed call_read and call_write endpoint and added a form to easily test remote calls

--- apps/remotecalls/views.py
+++ apps/remotecalls/views.py
@@ -10,16 +10,18 @@ except ImportError:
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 
 import requests
 from requests_oauthlib import OAuth2
 
+from .forms import JsonForm
+
 
 @login_required
 def call_read(request):
+    context = {'name': 'Authenticated Home'}
     # first we get the token used to login
     token = request.user.social_auth.get(provider='myoauth').access_token
     auth = OAuth2(settings.SOCIAL_AUTH_MYOAUTH_KEY,
@@ -27,23 +29,45 @@ def call_read(request):
     # next we call the remote api
     url = urljoin(settings.HHS_OAUTH_URL, '/api/read/')
     response = requests.get(url, auth=auth)
-    if response.status_code == 403:
-        return JsonResponse({'error': 'no read capability'}, status=403)
+    if response.status_code == 200:
+        content = response.json()
+    elif response.status_code == 403:
+        content = {'error': 'no read capability'}
     else:
-        return JsonResponse(response.json(), status=response.status_code)
+        content = {'error': 'server error'}
+
+    context['form'] = JsonForm()
+    context['remote_status_code'] = response.status_code
+    context['remote_content'] = content
+    return render(request, 'authenticated-home.html', context)
 
 
 @login_required
 @require_POST
 def call_write(request):
-    # first we get the token used to login
-    token = request.user.social_auth.get(provider='myoauth').access_token
-    auth = OAuth2(settings.SOCIAL_AUTH_MYOAUTH_KEY,
-                  token={'access_token': token, 'token_type': 'Bearer'})
-    # next we call the remote api
-    url = urljoin(settings.HHS_OAUTH_URL, '/api/write/')
-    response = requests.post(url, auth=auth, json=request.POST)
-    if response.status_code == 403:
-        return JsonResponse({'error': 'no write capability'}, status=403)
+    context = {'name': 'Authenticated Home'}
+
+    if request.method == 'POST':
+        form = JsonForm(request.POST)
+        if form.is_valid():
+            # first we get the token used to login
+            token = request.user.social_auth.get(provider='myoauth').access_token
+            auth = OAuth2(settings.SOCIAL_AUTH_MYOAUTH_KEY,
+                          token={'access_token': token, 'token_type': 'Bearer'})
+            # next we call the remote api
+            url = urljoin(settings.HHS_OAUTH_URL, '/api/write/')
+            json_data = json.loads(form.cleaned_data['json'])
+            response = requests.post(url, auth=auth, json=json_data)
+            if response.status_code == 200:
+                content = response.json()
+            elif response.status_code == 403:
+                content = {'error': 'no write capability'}
+            else:
+                content = {'error': 'server error'}
+            context['remote_status_code'] = response.status_code
+            context['remote_content'] = content
     else:
-        return JsonResponse(response.json(), status=response.status_code)
+        form = JsonForm()
+
+    context['form'] = form
+    return render(request, 'authenticated-home.html', context)
