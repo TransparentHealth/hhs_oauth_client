e2c41b89eba16bdc84e519784ba82bf3e5a2dd17
Federico Frenguelli <synasius@gmail.com>
2016-02-25 10:50:00 +0100

    added call_read and call_write endpoints

--- apps/remotecalls/urls.py
+++ apps/remotecalls/urls.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
 
 from django.conf.urls import patterns, include, url
-from views import *
+
+from .views import *
 
 
 urlpatterns = patterns('',
-
     url(r'call-read', call_read,  name="call_read"),
+    url(r'call-write', call_write,  name="call_write"),
 )
--- apps/remotecalls/views.py
+++ apps/remotecalls/views.py
@@ -1,17 +1,49 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: ai ts=4 sts=4 et sw=4
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 import json
 
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
 
-# Create your views here.
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_POST
+
+import requests
+from requests_oauthlib import OAuth2
+
 
 @login_required
 def call_read(request):
-    return JsonResponse({"foo":"bar"})
-    
-    
-    
+    # first we get the token used to login
+    token = request.user.social_auth.get(provider='myoauth').access_token
+    auth = OAuth2(settings.SOCIAL_AUTH_MYOAUTH_KEY,
+                  token={'access_token': token, 'token_type': 'Bearer'})
+    # next we call the remote api
+    url = urljoin(settings.HHS_OAUTH_URL, '/api/read/')
+    response = requests.get(url, auth=auth)
+    if response.status_code == 403:
+        return JsonResponse({'error': 'no read capability'}, status=403)
+    else:
+        return JsonResponse(response.json(), status=response.status_code)
+
+
+@login_required
+@require_POST
+def call_write(request):
+    # first we get the token used to login
+    token = request.user.social_auth.get(provider='myoauth').access_token
+    auth = OAuth2(settings.SOCIAL_AUTH_MYOAUTH_KEY,
+                  token={'access_token': token, 'token_type': 'Bearer'})
+    # next we call the remote api
+    url = urljoin(settings.HHS_OAUTH_URL, '/api/write/')
+    response = requests.post(url, auth=auth, json=request.POST)
+    if response.status_code == 403:
+        return JsonResponse({'error': 'no write capability'}, status=403)
+    else:
+        return JsonResponse(response.json(), status=response.status_code)
