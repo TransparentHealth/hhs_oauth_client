8eac4606df8cccb3247ae4c567eeecd7d1b31d77
Alan Viars <aviars@videntity.com>
2016-03-16 12:08:07 -0400

    removed unneeded import

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.conf import settings
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext_lazy as _
