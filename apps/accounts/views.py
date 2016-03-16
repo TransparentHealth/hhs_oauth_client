8e5fc55ec54e2819f7164a1778c7fdc468bee6ec
Alan Viars <aviars@videntity.com>
2016-03-16 12:06:17 -0400

    Added oAuth backend name to login button

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4
+from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.views.decorators.http import  require_GET
-
 from django.core.urlresolvers import reverse
 from django.contrib.auth import logout
 from django.contrib import messages
@@ -21,7 +21,7 @@ def my_logout(request):
 @require_GET    
 def my_login(request):
     #this is a GET
-    return render(request,'login.html', {})
+    return render(request,'login.html', {'PROPRIETARY_BACKEND_NAME': settings.PROPRIETARY_BACKEND_NAME})
 
 
 
