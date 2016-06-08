1f49a2a246957f4ae84e1cb2a1b916130d356841
will <will.flwrs@gmail.com>
2016-06-08 15:35:13 -0400

    Add functionality for fhir organization and practitioner

--- apps/provider/forms.py
+++ apps/provider/forms.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 import json
+from pdt import 
+from pdt import pdt
 
 class JsonForm(forms.Form):
     json = forms.CharField(label='JSON body', max_length=10000, widget=forms.Textarea,
@@ -20,11 +22,55 @@ class JsonForm(forms.Form):
             if type (j) != type({}):
                 msg=_("The field does not contain a valid JSON object.")
                 raise forms.ValidationError(msg)
-            
-        except ValueError:  
+
+        except ValueError:
             msg=_("The field does not contain valid JSON.")
             raise forms.ValidationError(msg)
-        
+
         return jsonf
-        
-    
+
+class PractitionerForm(forms.Form):
+    json = forms.CharField(label='JSON body', max_length=10000, widget=forms.Textarea,
+            help_text =_("This field must contain a Practitioner FHIR JSON object e.g. {}"))
+
+    required_css_class = 'required'
+
+
+    def clean_json(self):
+        jsonf  = self.cleaned_data.get('json')
+
+        try:
+            j = json.loads(jsonf)
+            json_pract_result = pdt.json_schema_check.json_schema_check(pdt.fhir_json_schema.fhir_practitioner_schema.json,j)
+            if json_pract_result['errors'] != []:
+                msg=_("The field does not contain a valid FHIR Practitioner JSON object: ", json_pract_result)
+                raise forms.ValidationError(msg)
+
+        except ValueError:
+            msg=_("The field does not contain valid JSON.")
+            raise forms.ValidationError(msg)
+
+        return jsonf
+
+class OrganizationForm(forms.Form):
+    json = forms.CharField(label='JSON body', max_length=10000, widget=forms.Textarea,
+            help_text =_("This field must contain an Organization FHIR JSON object e.g. {}"))
+
+    required_css_class = 'required'
+
+
+    def clean_json(self):
+        jsonf = self.cleaned_data.get('json')
+
+        try:
+            j = json.loads(jsonf)
+            json_pract_result = pdt.json_schema_check.json_schema_check(pdt.fhir_json_schema.fhir_organization_schema.json,j)
+            if json_pract_result['errors'] != []:
+                msg=_("The field does not contain a valid FHIR Organization JSON object: ", json_pract_result)
+                raise forms.ValidationError(msg)
+
+        except ValueError:
+            msg=_("The field does not contain valid JSON.")
+            raise forms.ValidationError(msg)
+
+        return jsonf
