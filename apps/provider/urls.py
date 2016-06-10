3581c3c71bdf3ff84961df4b328f0bfc2adf0bc7
will <will.flwrs@gmail.com>
2016-06-10 15:32:18 -0400

    Add url for update vs. push for pract and org

--- apps/provider/urls.py
+++ apps/provider/urls.py
@@ -10,5 +10,7 @@ urlpatterns = patterns('',
     url(r'^pjson/push$', pjson_provider_push,  name="pjson_provider_push"),
     url(r'^fhir/practitioner/push$', fhir_practitioner_push,  name="fhir_practitioner_push"),
     url(r'^fhir/organization/push$', fhir_organization_push,  name="fhir_organization_push"),
+    url(r'^fhir/practitioner/update$', fhir_practitioner_update,  name="fhir_practitioner_update"),
+    url(r'^fhir/organization/update$', fhir_organization_update,  name="fhir_organization_update"),
 
 )
