fec577c32844a863a2f17b659274ccc1ea31e1dd
will <will.flwrs@gmail.com>
2016-06-30 12:58:50 -0400

    Initial form commit

--- apps/provider/forms.py
+++ apps/provider/forms.py
@@ -19,7 +19,7 @@ class PractitionerModelForm(forms.ModelForm):
             self.fields['fhir_id'].widget.attrs['readonly'] = True
             self.fields['first_name'].widget.attrs['required'] = True
             self.fields['last_name'].widget.attrs['required'] = True
-    
+
     class Meta:
         model = Practitioner
         fields = ('npi','fhir_id','first_name', 'last_name',)
@@ -33,24 +33,25 @@ class FetchPractitionerForm(forms.Form):
     required_css_class = 'required'
     def clean_npi(self):
         npi = self.cleaned_data.get('npi')
-        url = "https://registry.npi.io/search/fhir/Practitioner.json?identifier.value=%s" % (npi)
+        url = "https://registry.npi.io/search/fhir/Practitioner." \
+            "json?identifier.value=%s" % (npi)
         response = requests.get(url)
         try:
             jr = json.loads(response.text)
-            
+
             if 'results' not in jr:
                 msg=_("The lookup failed. Invalid response from server")
                 raise forms.ValidationError(msg)
-            
+
             if not jr['results']:
                     msg=_("Invalid NPI")
-                    raise forms.ValidationError(msg) 
+                    raise forms.ValidationError(msg)
         except ValueError:
-            msg=_("The lookup failed. JSON was not returned bt the server.")
+            msg=_("The lookup failed. JSON was not returned by the server.")
             raise forms.ValidationError(msg)
-        return npi 
-        
-        
+        return npi
+
+
 
 class AddressForm(forms.Form):
     line_1= forms.CharField(max_length=255)
@@ -74,27 +75,46 @@ class AddressForm(forms.Form):
         json_fhir_dict['country'] = self.cleaned_data.get('country')
         json_fhir_dict['use'] = self.cleaned_data.get('use')
         return json.dumps(json_fhir_dict, indent=4)
-        
+
+class AffiliationForm(forms.Form):
+    purpose = forms.ChoiceField(choices =
+                                    (('PROVIDER-NETWORK', 'Provider-Network'),
+                                    ('MEDICARE-NETWORK', 'Medicare-Network')))
+    npi = forms.CharField(max_length=10, label="Organization's NPI (Type 2)")
+    endpoint_data_type = forms.ChoiceField(
+                choices = (('DIRECT-EMAIL-ADDRESS', 'Direct-Email-Address')),
+                required=False)
+    endpoint = forms.CharField(max_length=512, required=False)
+    required_css_class = 'required'
+
+    def create_fhir_json(self):
+        json_fhir_dict = OrderedDict()
+        json_fhir_dict['purpose'] = self.cleaned_data.get('purpose')
+        json_fhir_dict['npi'] = self.cleaned_data.get('npi')
+        json_fhir_dict['endpoint_data_type'] = self.cleaned_data.get('endpoint_data_type)')
+        json_fhir_dict['endpoint'] = self.cleaned_data.get('endpoint')
+
+        return json.dumps(json_fhir_dict, indent=4)
 
 
 class PractitionerHumanForm(forms.Form):
     first_name = forms.CharField(max_length=255)
     last_name  = forms.CharField(max_length=255)
-    
-    
+
+
     required_css_class = 'required'
 
 
 class OrganizationHumanForm(forms.Form):
     organization_name = forms.CharField(max_length=255)
-    
+
     required_css_class = 'required'
 
 
 
-
 class JsonForm(forms.Form):
-    json = forms.CharField(label='JSON body', max_length=10000, widget=forms.Textarea,
+    json = forms.CharField(label='JSON body', max_length=10000,
+            widget=forms.Textarea,
             help_text =_("This field must contain a JSON object e.g. {}"))
 
     required_css_class = 'required'
@@ -116,8 +136,10 @@ class JsonForm(forms.Form):
         return jsonf
 
 class PractitionerForm(forms.Form):
-    json = forms.CharField(label='JSON body', max_length=10000, widget=forms.Textarea,
-            help_text =_("This field must contain a Practitioner FHIR JSON object e.g. {}"))
+    json = forms.CharField(label='JSON body',
+            max_length=10000, widget=forms.Textarea,
+            help_text =_("This field must contain a Practitioner" \
+                         "FHIR JSON object e.g. {}"))
 
     required_css_class = 'required'
 
@@ -126,9 +148,11 @@ class PractitionerForm(forms.Form):
         jsonf  = self.cleaned_data.get('json')
         try:
             j = json.loads(jsonf)
-            json_pract_result = json_schema_check_fhir.json_schema_check_fhir('Practitioner', jsonf)
+            json_pract_result = json_schema_check_fhir.json_schema_check_fhir(
+                'Practitioner', jsonf)
             if json_pract_result['errors'] != []:
-                msg=_("The field does not contain a valid FHIR Practitioner JSON object: ", json_pract_result['errors'])
+                msg=_("The field does not contain a valid FHIR Practitioner" \
+                      "JSON object: ", json_pract_result['errors'])
                 raise forms.ValidationError(msg)
 
         except ValueError:
@@ -138,8 +162,10 @@ class PractitionerForm(forms.Form):
         return jsonf
 
 class OrganizationForm(forms.Form):
-    json = forms.CharField(label='JSON body', max_length=10000, widget=forms.Textarea,
-            help_text =_("This field must contain an Organization FHIR JSON object e.g. {}"))
+    json = forms.CharField(label='JSON body', max_length=10000,
+            widget=forms.Textarea,
+            help_text =_("This field must contain an Organization FHIR JSON \
+                         object e.g. {}"))
 
     required_css_class = 'required'
 
@@ -148,9 +174,12 @@ class OrganizationForm(forms.Form):
         jsonf = self.cleaned_data.get('json')
         try:
             j = json.loads(jsonf)
-            json_org_result = json_schema_check_fhir.json_schema_check_fhir('Organization', jsonf)
+            json_org_result = json_schema_check_fhir.json_schema_check_fhir(
+                                                                'Organization',
+                                                                jsonf)
             if json_org_result['errors'] != []:
-                msg=_("The field does not contain a valid FHIR Organization JSON object: ", json_org_result['errors'])
+                msg=_("The field does not contain a valid FHIR Organization \
+                      JSON object: ", json_org_result['errors'])
                 raise forms.ValidationError(msg)
 
         except ValueError:
